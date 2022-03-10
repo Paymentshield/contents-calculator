2f9230db87efff5896f7a8d875fdeeb0bda28a0c
Hamid Hosseini <95850737+hhosseinipaymentshield@users.noreply.github.com>
2022-03-10 09:23:31 +0000

    Add netlify adaptor

--- svelte.config.js
+++ svelte.config.js
@@ -4,11 +4,7 @@ import adapter from '@sveltejs/adapter-auto';
 const config = {
 	kit: {
 		adapter: adapter(),
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
+		target: '#svelte',
 	}
 };
 
